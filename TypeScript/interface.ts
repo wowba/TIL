951f0d29d8c8e1c1151a35ac51b07cbc101a950f
wowba <leeyuwk54@gmail.com>
2022-07-22 16:35:24 +0900

    Update interface.ts

--- TypeScript/interface.ts
+++ TypeScript/interface.ts
@@ -31,3 +31,18 @@ function addTodo(todo: Todo) {
 const newTodo: Todo = { id: 1, content: `typescript`, completed: false };
 addTodo(newTodo);
 console.log(todos);
+
+// 인터페이스는 함수의 타입으로 사용할 수 있다.
+// 이때 함수의 인터페이스에는 타입이 선언된 파라미터 리스트와 리턴 타입을 정의한다.
+
+// 함수 인터페이스 정의
+interface SquareFunc {
+  (num: number): number;
+}
+
+// 함수 인터페이스를 구현하는 함수는 인터페이스를 준수해야 함.
+const squareFunc: SquareFunc = function (num: number) {
+  return num * num;
+}
+
+console.log(squareFunc(10))
